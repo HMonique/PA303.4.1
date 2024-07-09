d4a0199c9582c6fe8a02a9cf823bd24727320e77
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-09 10:44:00 -0400

    Commit 3

---
+++ src/Part2.java
@@ -0,0 +1,23 @@
+public class Part2 {
+    public static void main(String[] args) {
+//        int x = 10;
+//
+//        if (x > 10) {
+//            System.out.println("x is greater than 10");
+//
+//        } else {
+//            System.out.println("x is NOT greater than 10");
+//        }
+//    }
+//}
+
+        int x = 15;
+
+        if (x > 15) {
+            System.out.println("x is greater than 15");
+
+        } else {
+            System.out.println("x is NOT greater than 15");
+        }
+    }
+}
