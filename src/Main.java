f97e34f7201b01ddf07b0cd04fa06b4c7c8f44e9
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-09 10:30:19 -0400

    Commit 2

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        int x = 7;
+//        if (x > 10);
+//        System.out.println("Less than 10");
+
+        int x = 15;
+        if (x > 10);
+        System.out.println("Less than 10");
+    }
+}
