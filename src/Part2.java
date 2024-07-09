cce0fdb14d835521b8d29c64f1e0e0609b65d57e
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-09 13:18:56 -0400

    Commit 5

--- src/Part2.java
+++ src/Part2.java
@@ -1,6 +1,17 @@
 public class Part2 {
     public static void main(String[] args) {
-        int x = 15;
+//        int x = 15;
+//        if (x < 10) {
+//            System.out.println("x is less than 10");
+//        } else if (x > 10) {
+//            System.out.println("Between 10 and 20");
+//        } else {
+//            System.out.println("Greater than or equal to 20");
+//        }
+//    }
+//}
+
+        int x = 50;
         if (x < 10) {
             System.out.println("x is less than 10");
         } else if (x > 10) {
